52df57bef63fd3747457866ae44b8de87173d54d
Kaemiemi <131779342+Kaemiemi@users.noreply.github.com>
2024-11-14 15:52:00 -0300

    Cria objeto VibrationMotor

--- vibration_motor.py
+++ vibration_motor.py
@@ -1,32 +1,46 @@
-import machine
+from machine import Pin, PWM
 import time
 
-# Set the GPIO pin for PWM control
-motor_pin = machine.Pin(5, machine.Pin.OUT)  # You can change the GPIO number
+class VibrationMotor:
+    
+    def __init__(self, pin):
+        
+        # Set the GPIO pin for PWM control
+        self.motor_pin = Pin(pin, Pin.OUT) # You can change the GPIO number
 
-# Create PWM object
-pwm = machine.PWM(motor_pin)
+        # Create PWM object
+        self.pwm = PWM(self.motor_pin)
 
-# Set PWM frequency (motor can typically run between 500Hz to 2kHz)
-pwm.freq(1000)  # 1 kHz frequency
+        # Set PWM frequency (motor can typically run between 500Hz to 2kHz)
+        self.pwm.freq(1000)  # 1 kHz frequency
 
-# Set PWM duty cycle (range 0-1023)
-# 0 is off, 1023 is full speed
-def set_motor_speed(speed):
-    pwm.duty(speed)
+    # Set PWM duty cycle (range 0-1023)
+    # 0 is off, 1023 is full speed
+    def set_motor_speed(self, speed):
+        self.pwm.duty(speed)
+       
+    # Turn the motor on at full speed
+    def on(self):
+        print("Motor on")
+        self.set_motor_speed(1023)
+        
+    # Turn the motor off
+    def off(self):
+        print("Motor off")
+        self.set_motor_speed(0)
+        
+vibration_motor = VibrationMotor(5)
 
 try:
     while True:
-        # Turn the motor on at full speed
-        print("Motor on")
-        set_motor_speed(1023)
+        
+        vibration_motor.on()
         time.sleep(2)
 
         # Turn the motor off
-        print("Motor off")
-        set_motor_speed(0)
+        vibration_motor.off()
         time.sleep(2)
 
 except KeyboardInterrupt:
-    pwm.deinit()  # Stop PWM when exiting
+    vibration_motor.pwm.deinit()  # Stop PWM when exiting
     print("Program stopped")
