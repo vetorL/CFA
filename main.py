de81c02dccb86f6cd523bb2bb4c4f6c396a38015
K(M)² <131779342+Kaemiemi@users.noreply.github.com>
2024-11-20 18:56:29 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from vibration_motor import VibrationMotor
 from LDR import LDR
 from touch import touch
 from UV import UV
+from dados import Dados
 import time
 
 
@@ -20,6 +21,9 @@ class SolarCap:
 
         # inicializa o sensor de UV
         self.UV_sensor = UV(34)
+        
+        # Lista com os últimos 900 valores (equivalente aos últimos 15 minutos)
+        self.dados = Dados(900)
 
     
     def start(self):
@@ -27,21 +31,28 @@ class SolarCap:
         try:
             # Main loop do código
             while True:
-                # adicinar para ativar um timer quando a media do LDR > 95 && media do UV > 5
-                if self.ldr.value() == 100:
-                    # Liga o motor de vibração
-                    self.vibration_motor.on()    
-                else:
-                    # Desliga o motor de vibração
-                    self.vibration_motor.off()
                 
-                self.ldr.mostrar_luminosidade()
+                # Caso os dados tenham uma média maior que 90, ligar o motor de vibração até que ele
+                # seja manualmente desligado pelo usuário
+                if self.dados.getAverage() > 90:
+                    self.vibration_motor.on()
+                else:
+                    self.vibration_motor.off()
+                    
+                # Caso o usuário aperte o botão touch, resetar Dados (consequentemente desligará o motor caso esteja ligado)
+                if self.touch.is_touching() == True:
+                    self.dados.reset()
+                
+                # Ajusta os LEDs do LDR conforme o valor atual da luminosidade
                 self.ldr.adjust_LEDs()
                 
-                time.sleep(1)
+                # Adiciona dados de luminosidade da iteração atual em Dados
+                self.dados.add(self.ldr.value())
                 
-                if(self.touch.is_touching() == True):
-                    self.vibration_motor.pwm.deinit()
+                print(self.dados.getAverage())
+                
+                # timer de 1 segundo entre iterações do loop
+                time.sleep(1)
 
         except KeyboardInterrupt:
             self.vibration_motor.pwm.deinit()  # Stop PWM when exiting
