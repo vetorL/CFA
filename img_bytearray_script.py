d1989a80acde980dd5e9554e2c8f97989c695fb2
vetorL <145482274+vetorL@users.noreply.github.com>
2024-11-20 22:42:44 -0300

    Faz converter arquivos na ordem correta

--- img_bytearray_script.py
+++ img_bytearray_script.py
@@ -1,6 +1,7 @@
 import os
 from io import BytesIO
 from PIL import Image
+import re
 
 # Fixed size for resizing
 x, y = 128, 64  # Adjust dimensions as needed
@@ -18,9 +19,20 @@ with open(output_file, "w") as f:
 # Initialize image list
 img_list = []
 
+
 # Process each image in the assets directory
 img_counter = 1
-for filename in os.listdir(assets_dir):
+
+
+def natural_sort_key(s):
+    return [
+        int(text) if text.isdigit() else text.lower() for text in re.split("(\d+)", s)
+    ]
+
+
+for filename in sorted(os.listdir(assets_dir), key=natural_sort_key):
+    print("Sorted filenames:", sorted(os.listdir(assets_dir), key=natural_sort_key))
+
     file_path = os.path.join(assets_dir, filename)
 
     # Check if the file is an image
@@ -49,6 +61,6 @@ for filename in os.listdir(assets_dir):
 
 # Write the list of images to the Python file
 with open(output_file, "a") as f:
-    f.write(f"img_list = [{', '.join(img_list)}]\n")
+    f.write(f"images_list = [{', '.join(img_list)}]\n")
 
 print(f"Processed images saved to {output_file}.")
